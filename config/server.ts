7497bc2939d4224b6f1a291f231dac55072ade50
luong <luongtranhieu2@gmail.com>
2025-05-24 11:24:04 +0700

    update server.ts to use the new config structure

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env('PORT', 1337),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env('APP_KEYS'),
+    keys: env.array('APP_KEYS'),
   },
 });
